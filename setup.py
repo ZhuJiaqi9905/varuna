b1837130b8b73e64c46eebe4a9abbfdbbf0bca7c
ZhuJiaqi <zhujiaqi9905@gmail.com>
2024-06-18 13:28:13 +0800

    try to run profile and autoconfig

--- setup.py
+++ setup.py
@@ -8,10 +8,6 @@ subprocess.run(cmd, cwd=varuna_dir, check=True)
 tools_dir = os.path.join(this_dir, "tools", "simulator")
 cmd = ["g++","-std=c++11", "simulate-varuna-main.cc", "generate_schedule.cc", "simulate-varuna.cc", "-o", "simulate-varuna"]
 subprocess.run(cmd, cwd=tools_dir, check=True)
-cmd = ["cp","-r", "/workspace/varuna/tools", "/opt/conda/lib/python3.6/site-packages/varuna-0.0.1-py3.6.egg"]
-subprocess.run(cmd, cwd=tools_dir, check=True)
-cmd = ["cp", "/workspace/varuna/varuna/genschedule", "/opt/conda/lib/python3.6/site-packages/varuna-0.0.1-py3.6.egg/varuna/"]
-subprocess.run(cmd, cwd=tools_dir, check=True)
 setup(
     name="varuna",
     version="0.0.1",
@@ -21,3 +17,7 @@ setup(
     keywords='deep learning microsoft research pipelining',
     packages=['varuna']
 )
+cmd = ["cp","-r", "/workspace/varuna/tools", "/opt/conda/lib/python3.6/site-packages/varuna-0.0.1-py3.6.egg"]
+subprocess.run(cmd, cwd=tools_dir, check=True)
+cmd = ["cp", "/workspace/varuna/varuna/genschedule", "/opt/conda/lib/python3.6/site-packages/varuna-0.0.1-py3.6.egg/varuna/"]
+subprocess.run(cmd, cwd=tools_dir, check=True)
