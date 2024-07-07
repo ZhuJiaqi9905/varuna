183d0a8449f8cb56e02bf1e38a81c9106666e34b
ZhuJiaqi <zhujiaqi9905@gmail.com>
2024-07-07 19:59:08 +0800

    try to fix oom bug

--- varuna/auto_config.py
+++ varuna/auto_config.py
@@ -37,6 +37,8 @@ class AutoConfig:
             # get highest micro batch for each num_stage_cand
             mbs = self.get_microbatch_size(pp_size)
             print(f"Predicted microbatch size for {pp_size}: {mbs}")
+            if mbs < 0:
+                continue
             self.micro_batch[pp_size] = mbs
             dp_size = num_gpus // pp_size
             cuts_per_stage = self.num_pstages // pp_size
